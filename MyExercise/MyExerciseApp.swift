5880963a58f68c5e4eddf10c7d8933bdcec13b63
haju <haju0321@gmail.com>
2021-12-04 13:26:51 +0900

    Initial Commit

---
+++ MyExercise/MyExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyExerciseApp.swift
+//  MyExercise
+//
+//  Created by haju Kim on 2021/12/04.
+//
+
+import SwiftUI
+
+@main
+struct MyExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
